acd5d0501cc035b20cd63f17818049a5c15f08bb
Aleksander Styrmoe <astyrmoe@gmail.com>
2022-06-13 04:04:17 +0200

    batch job added

---
+++ src/main/java/edu/ntnu/alekssty/master/dataoperations/ResultJobBatch.java
@@ -0,0 +1,148 @@
+package edu.ntnu.alekssty.master.dataoperations;
+
+import edu.ntnu.alekssty.master.batch.Methods;
+import org.apache.flink.api.common.RuntimeExecutionMode;
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.functions.AggregateFunction;
+import org.apache.flink.api.java.tuple.Tuple5;
+import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.connector.file.src.FileSource;
+import org.apache.flink.core.fs.FileSystem;
+import org.apache.flink.core.fs.Path;
+import org.apache.flink.formats.csv.CsvReaderFormat;
+import org.apache.flink.ml.common.datastream.EndOfStreamWindows;
+import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+import java.io.File;
+
+public class ResultJobBatch {
+
+    public static void main(String[] args) throws Exception {
+
+        ParameterTool parameter = ParameterTool.fromArgs(args);
+
+        int k = parameter.getInt("k", 2);
+        Methods method = Methods.valueOf(parameter.get("method", "naive").toUpperCase());
+
+
+        String outputsPath = parameter.get("outputs-path", "/tmp/experiment-results/");
+        String job = parameter.get("job", "offlineB");
+
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment()
+                .setRuntimeMode(RuntimeExecutionMode.BATCH)
+                .setParallelism(1);
+        CsvReaderFormat<SomePojo> csvFormat = CsvReaderFormat.forPojo(SomePojo.class);
+
+        int i = 0;
+        DataStream<Tuple5<String, Integer, Integer, Integer, Integer>> r0 = extracted(outputsPath, method, job, i, csvFormat, env);
+        i++;
+        DataStream<Tuple5<String, Integer, Integer, Integer, Integer>> r1 = extracted(outputsPath, method, job, i, csvFormat, env);
+        i++;
+        DataStream<Tuple5<String, Integer, Integer, Integer, Integer>> r2 = extracted(outputsPath, method, job, i, csvFormat, env);
+        i++;
+        DataStream<Tuple5<String, Integer, Integer, Integer, Integer>> r3 = extracted(outputsPath, method, job, i, csvFormat, env);
+        i++;
+        DataStream<Tuple5<String, Integer, Integer, Integer, Integer>> r4 = extracted(outputsPath, method, job, i, csvFormat, env);
+        i++;
+        DataStream<Tuple5<String, Integer, Integer, Integer, Integer>> r5 = extracted(outputsPath, method, job, i, csvFormat, env);
+        i++;
+        DataStream<Tuple5<String, Integer, Integer, Integer, Integer>> r6 = extracted(outputsPath, method, job, i, csvFormat, env);
+        i++;
+        DataStream<Tuple5<String, Integer, Integer, Integer, Integer>> r7 = extracted(outputsPath, method, job, i, csvFormat, env);
+        i++;
+        DataStream<Tuple5<String, Integer, Integer, Integer, Integer>> r8 = extracted(outputsPath, method, job, i, csvFormat, env);
+        i++;
+        DataStream<Tuple5<String, Integer, Integer, Integer, Integer>> r9 = extracted(outputsPath, method, job, i, csvFormat, env);
+        i++;
+        DataStream<Tuple5<String, Integer, Integer, Integer, Integer>> r10 = extracted(outputsPath, method, job, i, csvFormat, env);
+        i++;
+        DataStream<Tuple5<String, Integer, Integer, Integer, Integer>> r11 = extracted(outputsPath, method, job, i, csvFormat, env);
+        i++;
+        DataStream<Tuple5<String, Integer, Integer, Integer, Integer>> r12 = extracted(outputsPath, method, job, i, csvFormat, env);
+        i++;
+        DataStream<Tuple5<String, Integer, Integer, Integer, Integer>> r13 = extracted(outputsPath, method, job, i, csvFormat, env);
+        i++;
+        DataStream<Tuple5<String, Integer, Integer, Integer, Integer>> r14 = extracted(outputsPath, method, job, i, csvFormat, env);
+        i++;
+        DataStream<Tuple5<String, Integer, Integer, Integer, Integer>> r15 = extracted(outputsPath, method, job, i, csvFormat, env);
+        i++;
+        DataStream<Tuple5<String, Integer, Integer, Integer, Integer>> r16 = extracted(outputsPath, method, job, i, csvFormat, env);
+        i++;
+        DataStream<Tuple5<String, Integer, Integer, Integer, Integer>> r17 = extracted(outputsPath, method, job, i, csvFormat, env);
+        i++;
+        DataStream<Tuple5<String, Integer, Integer, Integer, Integer>> r18 = extracted(outputsPath, method, job, i, csvFormat, env);
+        i++;
+        DataStream<Tuple5<String, Integer, Integer, Integer, Integer>> r19 = extracted(outputsPath, method, job, i, csvFormat, env);
+        i++;
+        DataStream<Tuple5<String, Integer, Integer, Integer, Integer>> r20 = extracted(outputsPath, method, job, i, csvFormat, env);
+        i++;
+        DataStream<Tuple5<String, Integer, Integer, Integer, Integer>> r21 = extracted(outputsPath, method, job, i, csvFormat, env);
+        i++;
+
+        r0.union(r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13, r14, r15, r16, r17, r18, r19, r20, r21)
+                        .writeAsCsv(outputsPath +"batch/"+ method + "-" + job + "-accumulated"+ "EVRYTHING" +".csv", FileSystem.WriteMode.OVERWRITE);
+
+
+        System.out.println(i);
+
+        env.execute();
+    }
+
+    private static DataStream<Tuple5<String, Integer, Integer, Integer, Integer>> extracted(String outputsPath, Methods method, String job, int i, CsvReaderFormat<SomePojo> csvFormat, StreamExecutionEnvironment env) {
+        String inputPointPath = outputsPath + "batch/" + method + "-" + job + "-points"+ i +".csv";
+        FileSource<SomePojo> source = FileSource.forRecordStreamFormat(csvFormat, Path.fromLocalFile(new File(inputPointPath))).build();
+        DataStream<Tuple5<String, Integer, Integer, Integer, Integer>> aggregated = env.fromSource(source, WatermarkStrategy.noWatermarks(), "Source")
+                .keyBy(t -> t.domain)
+                .window(EndOfStreamWindows.get())
+                .aggregate(new Aggregate());
+        aggregated.writeAsCsv(outputsPath +"batch/"+ method + "-" + job + "-accumulated"+ i +".csv", FileSystem.WriteMode.OVERWRITE);
+        return aggregated;
+    }
+
+    @JsonPropertyOrder({"domain", "assigned", "label"})
+    private static class SomePojo {
+        public String domain;
+        public int assigned;
+        public String label;
+    }
+
+    private static class Aggregate implements AggregateFunction<SomePojo, Tuple5<String, Integer, Integer, Integer, Integer>, Tuple5<String, Integer, Integer, Integer, Integer>> {
+        @Override
+        public Tuple5<String, Integer, Integer, Integer, Integer> createAccumulator() {
+            return Tuple5.of(null, 0, 0, 0, 0);
+        }
+
+        @Override
+        public Tuple5<String, Integer, Integer, Integer, Integer> add(SomePojo somePojo, Tuple5<String, Integer, Integer, Integer, Integer> acc) {
+            int cA = 0;
+            int cB = 0;
+            int cAn = 0;
+            int cBn = 0;
+            if (somePojo.assigned == 0) {
+                cA = 1;
+                if (somePojo.label.equals("normal")) {
+                    cAn = 1;
+                }
+            }
+            if (somePojo.assigned == 1) {
+                cB = 1;
+                if (somePojo.label.equals("normal")) {
+                    cBn = 1;
+                }
+            }
+            return Tuple5.of(somePojo.domain, acc.f1 + cA, acc.f2 + cAn, acc.f3 + cB, acc.f4 + cBn);
+        }
+
+        @Override
+        public Tuple5<String, Integer, Integer, Integer, Integer> getResult(Tuple5<String, Integer, Integer, Integer, Integer> stringIntegerIntegerIntegerIntegerTuple5) {
+            return stringIntegerIntegerIntegerIntegerTuple5;
+        }
+
+        @Override
+        public Tuple5<String, Integer, Integer, Integer, Integer> merge(Tuple5<String, Integer, Integer, Integer, Integer> acc0, Tuple5<String, Integer, Integer, Integer, Integer> acc1) {
+            return Tuple5.of(acc0.f0, acc0.f1 + acc1.f1, acc0.f2 + acc1.f2, acc0.f3 + acc1.f3, acc0.f4 + acc1.f4);
+        }
+    }
+}
--- src/main/java/edu/ntnu/alekssty/master/moo/OfflineTestJob.java
+++ src/main/java/edu/ntnu/alekssty/master/moo/OfflineTestJob.java
@@ -1,8 +1,156 @@
 package edu.ntnu.alekssty.master.moo;
 
-public class OfflineTestJob {
-    public static void main(String[] args) {
+import edu.ntnu.alekssty.master.batch.KMeansOfflineImprovements;
+import edu.ntnu.alekssty.master.batch.Methods;
+import edu.ntnu.alekssty.master.utils.CentroidToTupleForFileOperator;
+import edu.ntnu.alekssty.master.utils.Counter;
+import edu.ntnu.alekssty.master.utils.PointsToTupleForFileOperator;
+import edu.ntnu.alekssty.master.utils.StreamNSLKDDConnector;
+import edu.ntnu.alekssty.master.vectorobjects.Centroid;
+import edu.ntnu.alekssty.master.vectorobjects.Point;
+import org.apache.flink.api.common.JobExecutionResult;
+import org.apache.flink.api.common.RuntimeExecutionMode;
+import org.apache.flink.api.common.typeinfo.TypeHint;
+import org.apache.flink.api.common.typeinfo.TypeInformation;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.tuple.Tuple3;
+import org.apache.flink.api.java.tuple.Tuple4;
+import org.apache.flink.api.java.tuple.Tuple5;
+import org.apache.flink.api.java.typeutils.TupleTypeInfo;
+import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.core.fs.FileSystem;
+import org.apache.flink.iteration.DataStreamList;
+import org.apache.flink.ml.common.datastream.EndOfStreamWindows;
+import org.apache.flink.ml.linalg.DenseVector;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.windowing.ProcessAllWindowFunction;
+import org.apache.flink.streaming.api.windowing.windows.GlobalWindow;
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
+import org.apache.flink.util.Collector;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class OfflineTestJob {
+    public static void main(String[] args) throws Exception {
+
+        ParameterTool parameter = ParameterTool.fromArgs(args);
+
+        String inputPointPath = parameter.get("input-point-path", "/home/aleks/dev/master/NSL-KDD/KDDTest+.txt");
+        String outputsPath = parameter.get("outputs-path", "/tmp/experiment-results/");
+
+        int k = parameter.getInt("k", 2);
+        Methods method = Methods.valueOf(parameter.get("method", "naive").toUpperCase());
+        String job = "offlineB";
+        int seedForRnd = parameter.getInt("seed-rnd-input", 0);
+
+
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment().setParallelism(1);
+
+        StreamNSLKDDConnector source = new StreamNSLKDDConnector(inputPointPath, env);
+        source.connect();
+
+        KMeansOfflineImprovements engine = new KMeansOfflineImprovements()
+                .setK(k)
+                .setMaxIter(20);
+
+        DataStream<Tuple3<String, DenseVector, String>> testSource;
+        if (seedForRnd==0) {
+            testSource = new StreamNSLKDDConnector(inputPointPath, env).connect().getPoints();
+        } else {
+            testSource = new StreamNSLKDDConnector(inputPointPath, env).connect().getRandomPoints(seedForRnd);
+        }
+
+        DataStream<Tuple2<Integer, Tuple3<String, DenseVector, String>>> taggedInput = testSource.countWindowAll(1000).process(new ProcessAllWindowFunction<Tuple3<String, DenseVector, String>, Tuple2<Integer, Tuple3<String, DenseVector, String>>, GlobalWindow>() {
+            int i = 0;
+            @Override
+            public void process(ProcessAllWindowFunction<Tuple3<String, DenseVector, String>, Tuple2<Integer, Tuple3<String, DenseVector, String>>, GlobalWindow>.Context context, Iterable<Tuple3<String, DenseVector, String>> iterable, Collector<Tuple2<Integer, Tuple3<String, DenseVector, String>>> collector) throws Exception {
+                for (Tuple3<String, DenseVector, String> stringDenseVectorStringTuple3 : iterable) {
+                    collector.collect(Tuple2.of(i,stringDenseVectorStringTuple3));
+                }
+                i++;
+            }
+        }).setParallelism(1);
+
+        Map<Integer, DataStreamList> res = new HashMap<>();
+
+        for (int i = 0; i < 23 ; i++) {
+            int finalI = i;
+            DataStream<Tuple2<Integer, Tuple3<String, DenseVector, String>>> temp2 = taggedInput.filter(e -> e.f0 == finalI);
+            DataStream<Tuple3<String, DenseVector, String>> temp = temp2.map(t -> t.f1).returns(new TupleTypeInfo<>(TypeInformation.of(String.class), TypeInformation.of(DenseVector.class), TypeInformation.of(String.class)));
+            DataStreamList returned = engine.fit(temp, method);
+            res.put(i, returned);
+        }
+
+        for (Integer i : res.keySet()) {
+            DataStream<Point> resP = res.get(i).get(1);
+            resP.map(new PointsToTupleForFileOperator())
+/*
+                    .windowAll(EndOfStreamWindows.get())
+                    .process(new ProcessAllWindowFunction<Tuple3<String, Integer, String>, Tuple5<String, Integer, Integer, Integer, Integer>, TimeWindow>() {
+                        @Override
+                        public void process(ProcessAllWindowFunction<Tuple3<String, Integer, String>, Tuple5<String, Integer, Integer, Integer, Integer>, TimeWindow>.Context context, Iterable<Tuple3<String, Integer, String>> iterable, Collector<Tuple5<String, Integer, Integer, Integer, Integer>> collector) throws Exception {
+                            int tp = 0;
+                            int fp = 0;
+                            int tn = 0;
+                            int fn = 0;
+                            Map<String, Tuple4<Integer, Integer, Integer, Integer>> domainMap = new HashMap<>();
+                            for (Tuple3<String, Integer, String> point : iterable) {
+                                String domain = point.f0;
+                                Tuple4<Integer, Integer, Integer, Integer> tuple = domainMap.get(domain);
+                                if (point.f1 == 0) {
+                                    tuple.f0 ++;
+                                    if (point.f2.equals("normal")) {
+                                        tuple.f1 ++;
+                                    }
+                                }
+                                if (point.f1 == 1) {
+                                    tuple.f2 ++;
+                                    if (point.f2.equals("normal")) {
+                                        tuple.f3 ++;
+                                    }
+                                }
+                                domainMap.put(domain, tuple);
+                            }
+                            for (String domain : domainMap.keySet()) {
+                                Tuple4<Integer, Integer, Integer, Integer> tuple = domainMap.get(domain);
+                                boolean aNormal;
+                                if (tuple.f1 != tuple.f3) {
+                                    aNormal = tuple.f1>tuple.f3;
+                                } else {
+                                    aNormal = tuple.f0 > tuple.f2;
+                                }
+                                if (aNormal) {
+                                    tp += tuple.f2 - tuple.f3;
+                                    fp += tuple.f3;
+                                    tn += tuple.f1;
+                                    fn += tuple.f0 - tuple.f1;
+                                } else {
+                                    tp += tuple.f0 - tuple.f1;
+                                    fp += tuple.f1;
+                                    tn += tuple.f3;
+                                    fn += tuple.f2 - tuple.f3;
+                                }
+                            }
+                            double br = (tp+fn)/(tp+fp+tn+fn);
+                            double tpr = tp/(tp+fn);
+                            double fpr = fp/(fp+tn);
+                            double ppv = br*tpr/(br*tpr+(1-br)*fpr);
+                            System.out.println("TPR = " + tpr);
+                            System.out.println("FPR = " + fpr);
+                            System.out.println("PPV = " + ppv);
+                            collector.collect(Tuple5.of(String.valueOf(i), tp, fp, tn, fn));
+                        }
+                    })
+*/
+                    .writeAsCsv(outputsPath + "batch/" + method + "-" + job + "-points" + i + ".csv", FileSystem.WriteMode.OVERWRITE).setParallelism(1);
+        }
+
+        JobExecutionResult jobResult = env.execute("Experimental work");
+        System.out.println("JOB RESULTS:\n" + jobResult.getJobExecutionResult());
     }
 
 }
